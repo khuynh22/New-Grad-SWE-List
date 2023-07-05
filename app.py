fc0b9fe37839f811cefa7334c0fecc42848cd634
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-07-05 15:30:23 -0500

    Add styling to the planner

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+
 app = Flask(__name__)
 items = []
+
+
 @app.route('/')
 def checklist():
     return render_template('checklist.html', items=items)
+
 
 @app.route('/add', methods=['POST'])
 def add_item():
@@ -11,6 +15,7 @@ def add_item():
     items.append(item)
     # flash('Item added')
     return redirect('/')
+
 
 @app.route('/edit/<int:item_id>', methods=['GET', 'POST'])
 def edit_item(item_id):
@@ -20,8 +25,9 @@ def edit_item(item_id):
         new_item = request.form['item']
         items[item_id - 1] = new_item
         return redirect('/')
-    
+
     return render_template('edit.html', item=item, item_id=item_id)
+
 
 @app.route('/delete/<int:item_id>')
 def delete_item(item_id):
